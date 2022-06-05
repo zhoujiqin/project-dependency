90c867f8e67269e57e35ca0e26e966320cdbd733
root <root@localhost.localdomain>
2022-06-05 08:17:47 +0800

    test

---
+++ internal/v2/controller/http/common.go
@@ -0,0 +1,110 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+//
+// Copyright (C) 2020 IOTech Ltd
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package http
+
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+
+	bootstrapContainer "github.com/edgexfoundry/go-mod-bootstrap/bootstrap/container"
+	"github.com/edgexfoundry/go-mod-core-contracts/errors"
+
+	sdkCommon "github.com/edgexfoundry/device-sdk-go/internal/common"
+	"github.com/edgexfoundry/device-sdk-go/internal/container"
+	"github.com/edgexfoundry/device-sdk-go/internal/telemetry"
+
+	contractsV2 "github.com/edgexfoundry/go-mod-core-contracts/v2"
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/dtos/common"
+)
+
+// Ping handles the request to /ping endpoint. Is used to test if the service is working
+// It returns a response as specified by the V2 API swagger in openapi/v2
+func (c *V2HttpController) Ping(writer http.ResponseWriter, request *http.Request) {
+	response := common.NewPingResponse()
+	c.sendResponse(writer, request, contractsV2.ApiPingRoute, response, http.StatusOK)
+}
+
+// Version handles the request to /version endpoint. Is used to request the service's versions
+// It returns a response as specified by the V2 API swagger in openapi/v2
+func (c *V2HttpController) Version(writer http.ResponseWriter, request *http.Request) {
+	response := common.NewVersionSdkResponse(sdkCommon.ServiceVersion, sdkCommon.SDKVersion)
+	c.sendResponse(writer, request, contractsV2.ApiVersionRoute, response, http.StatusOK)
+}
+
+// Config handles the request to /config endpoint. Is used to request the service's configuration
+// It returns a response as specified by the V2 API swagger in openapi/v2
+func (c *V2HttpController) Config(writer http.ResponseWriter, request *http.Request) {
+	response := common.NewConfigResponse(container.ConfigurationFrom(c.dic.Get))
+	c.sendResponse(writer, request, contractsV2.ApiVersionRoute, response, http.StatusOK)
+}
+
+// Metrics handles the request to the /metrics endpoint, memory and cpu utilization stats
+// It returns a response as specified by the V2 API swagger in openapi/v2
+func (c *V2HttpController) Metrics(writer http.ResponseWriter, request *http.Request) {
+	telem := telemetry.NewSystemUsage()
+	metrics := common.Metrics{
+		MemAlloc:       telem.Memory.Alloc,
+		MemFrees:       telem.Memory.Frees,
+		MemLiveObjects: telem.Memory.LiveObjects,
+		MemMallocs:     telem.Memory.Mallocs,
+		MemSys:         telem.Memory.Sys,
+		MemTotalAlloc:  telem.Memory.TotalAlloc,
+		CpuBusyAvg:     uint8(telem.CpuBusyAvg),
+	}
+
+	response := common.NewMetricsResponse(metrics)
+	c.sendResponse(writer, request, contractsV2.ApiMetricsRoute, response, http.StatusOK)
+}
+
+// Secret handles the request to add Device Service exclusive secret to the Secret Store
+// It returns a response as specified by the V2 API swagger in openapi/v2
+func (c *V2HttpController) Secret(writer http.ResponseWriter, request *http.Request) {
+	defer func() {
+		_ = request.Body.Close()
+	}()
+
+	provider := bootstrapContainer.SecretProviderFrom(c.dic.Get)
+	secretRequest := common.SecretRequest{}
+	err := json.NewDecoder(request.Body).Decode(&secretRequest)
+	if err != nil {
+		edgexError := errors.NewCommonEdgeX(errors.KindContractInvalid, "JSON decode failed", err)
+		c.sendEdgexError(writer, request, edgexError, sdkCommon.APIV2SecretRoute)
+		return
+	}
+
+	path, secret := c.prepareSecret(secretRequest)
+
+	if err := provider.StoreSecrets(path, secret); err != nil {
+		edgexError := errors.NewCommonEdgeX(errors.KindServerError, "Storing secret failed", err)
+		c.sendEdgexError(writer, request, edgexError, sdkCommon.APIV2SecretRoute)
+		return
+	}
+
+	response := common.NewBaseResponse(secretRequest.RequestId, "", http.StatusCreated)
+	c.sendResponse(writer, request, sdkCommon.APIV2SecretRoute, response, http.StatusCreated)
+}
+
+func (c *V2HttpController) prepareSecret(request common.SecretRequest) (string, map[string]string) {
+	var secretKVs = make(map[string]string)
+	for _, secret := range request.SecretData {
+		secretKVs[secret.Key] = secret.Value
+	}
+
+	path := strings.TrimSpace(request.Path)
+	config := container.ConfigurationFrom(c.dic.Get)
+
+	// add '/' in the full URL path if it's not already at the end of the base path or sub path
+	if !strings.HasSuffix(config.SecretStore.Path, "/") && !strings.HasPrefix(path, "/") {
+		path = "/" + path
+	} else if strings.HasSuffix(config.SecretStore.Path, "/") && strings.HasPrefix(path, "/") {
+		// remove extra '/' in the full URL path because secret store's (Vault) APIs don't handle extra '/'.
+		path = path[1:]
+	}
+
+	return path, secretKVs
+}
---
+++ pkg/service/main.go
@@ -0,0 +1,85 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+//
+// Copyright (C) 2020 IOTech Ltd
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package service
+
+import (
+	"context"
+	"os"
+
+	"github.com/edgexfoundry/device-sdk-go/internal/autodiscovery"
+	"github.com/edgexfoundry/device-sdk-go/internal/clients"
+	"github.com/edgexfoundry/device-sdk-go/internal/common"
+	"github.com/edgexfoundry/device-sdk-go/internal/container"
+
+	"github.com/edgexfoundry/go-mod-bootstrap/bootstrap"
+	"github.com/edgexfoundry/go-mod-bootstrap/bootstrap/flags"
+	"github.com/edgexfoundry/go-mod-bootstrap/bootstrap/handlers"
+	"github.com/edgexfoundry/go-mod-bootstrap/bootstrap/interfaces"
+	"github.com/edgexfoundry/go-mod-bootstrap/bootstrap/startup"
+	"github.com/edgexfoundry/go-mod-bootstrap/di"
+
+	"github.com/gorilla/mux"
+)
+
+var instanceName string
+
+func Main(serviceName string, serviceVersion string, proto interface{}, ctx context.Context, cancel context.CancelFunc, router *mux.Router) {
+	startupTimer := startup.NewStartUpTimer(serviceName)
+
+	additionalUsage :=
+		"    -i, --instance                  Provides a service name suffix which allows unique instance to be created\n" +
+			"                                    If the option is provided, service name will be replaced with \"<name>_<instance>\"\n"
+	sdkFlags := flags.NewWithUsage(additionalUsage)
+	sdkFlags.FlagSet.StringVar(&instanceName, "instance", "", "")
+	sdkFlags.FlagSet.StringVar(&instanceName, "i", "", "")
+	sdkFlags.Parse(os.Args[1:])
+
+	serviceName = setServiceName(serviceName)
+	ds = &DeviceService{}
+	ds.Initialize(serviceName, serviceVersion, proto)
+
+	dic := di.NewContainer(di.ServiceConstructorMap{
+		container.ConfigurationName: func(get di.Get) interface{} {
+			return ds.config
+		},
+	})
+
+	httpServer := handlers.NewHttpServer(router, true)
+
+	bootstrap.Run(
+		ctx,
+		cancel,
+		sdkFlags,
+		ds.ServiceName,
+		common.ConfigStemDevice+common.ConfigMajorVersion,
+		ds.config,
+		startupTimer,
+		dic,
+		[]interfaces.BootstrapHandler{
+			handlers.SecureProviderBootstrapHandler,
+			httpServer.BootstrapHandler,
+			clients.NewClients().BootstrapHandler,
+			NewBootstrap(router).BootstrapHandler,
+			autodiscovery.BootstrapHandler,
+			handlers.NewStartMessage(serviceName, serviceVersion).BootstrapHandler,
+		})
+
+	ds.Stop(false)
+}
+
+func setServiceName(name string) string {
+	envValue := os.Getenv(common.EnvInstanceName)
+	if len(envValue) > 0 {
+		instanceName = envValue
+	}
+
+	if len(instanceName) > 0 {
+		name = name + "_" + instanceName
+	}
+
+	return name
+}
